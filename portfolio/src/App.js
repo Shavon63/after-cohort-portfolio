88f4362cc1876813ffde4d8cf687f69c4aaceaab
Shavon63 <newgenesisnyc@gmail.com>
2023-03-21 01:43:23 -0400

    updated

--- portfolio/src/App.js
+++ portfolio/src/App.js
@@ -1,4 +1,4 @@
-import { Routes, Route, Router } from 'react-router-dom';
+import { Routes, Route } from 'react-router-dom';
 import Home from './pages/Home/Home';
 import Projects from './pages/Projects/Projects'
 import Experience from './pages/Experience/Experience';
