4c0f3c67569830f5e8ef36fe8f030e7868afef88
Shavon63 <newgenesisnyc@gmail.com>
2023-03-20 21:49:10 -0400

    im on

--- backend/server.js
+++ backend/server.js
@@ -6,9 +6,24 @@ const express = require("express")
 const app = express()
 const cors = require("cors")
 require("dotenv").config()
+// ... other imports ...
+
+const path = require("path")
 
 // cors allows our frontend to communicate with the backend
 app.use(cors())
+
+// ... other middleware ...
+
+// use the React build folder for static files
+app.use(express.static(path.join(path.dirname(__dirname), "frontend", "build")))
+
+// ... other routes/controllers ...
+
+// any other route not matching the routes above gets routed by React
+app.get("*", (req, res) => {
+    res.sendFile(path.join(path.dirname(__dirname), "frontend", "build", "index.html"));
+});
 
 
 // +-+-+-+-+-+-+-+-+
